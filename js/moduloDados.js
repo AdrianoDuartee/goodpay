cc913d816931d92afe00f9c36c0227061502ffbb
AdrianoDuartee <adrianoduarte6@gmail.com>
2022-10-03 17:17:43 -0300

    melhorias login/senha

---
+++ js/moduloDados.js
@@ -0,0 +1,38 @@
+/**
+ * dentro deste array existem usuários com esta estrutura
+ *  - id
+ * - nomeUsuario
+ * - senha
+ * - email
+ */
+
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: 'Adriano-Duarte',
+        senha: '12345',
+        email: 'adriano.duarte@gmail.com',
+    },
+    {
+        id: 2,
+        nomeUsuario: 'Pedro',
+        senha: '12345',
+        email: 'Pedro@gmail.com', 
+    },
+    {
+        id: 3,
+        nomeUsuario: 'Jennifer',
+        senha: '12345',
+        email: 'Jennifer@gmail.com',
+    },
+    {
+        id: 4,
+        nomeUsuario: 'João',
+        senha: '12345',
+        email: 'João@gmail.com',
+    }
+]
+export default usuarios;
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
